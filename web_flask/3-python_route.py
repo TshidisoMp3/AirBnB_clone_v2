92f7ffee37dd15490d36dec3a03a9632e0211400
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-05-31 11:00:24 +0200

    web_flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-# Script that starts a Flask web application
+"""Script that starts a Flask web application"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hihi_hbnb():
+    """Function to return Hello HBNB"""
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def Yelloz():
+    """Function to show/return HBNB"""
     return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
+def cfunc(text):
+    """Function to show/return C + text"""
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is cool'):
+def pythonCool_text(text='is cool'):
+    """Function to show/return Python + text"""
     return 'Python %s' % text.replace('_', ' ')
 
 if __name__ == "__main__":
