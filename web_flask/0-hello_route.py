77675e7bf813f4fe03632648ebf67cdc20a2d0ef
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-05-31 10:40:53 +0200

    web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# Script that starts a Flask web application
+"""Script that starts a Flask web application"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hihi_hbnb():
+    """Function to return Hello HBNB"""
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
